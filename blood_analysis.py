aefe2438fed9c3982bf333245e175e0995fb6cea
Natalie Wilkinson <nataliewilkinson236@gmail.com>
2020-05-26 11:34:21 -0400

    Added line generation exercise from class 5/26

--- blood_analysis.py
+++ blood_analysis.py
@@ -1,5 +1,6 @@
 # blood_analysis.py
 
+
 def LDL_analysis(LDL_result):
     if LDL_result < 130:
         return "Normal"
@@ -7,24 +8,27 @@ def LDL_analysis(LDL_result):
         return "Borderline High"
     elif 160 <= LDL_result < 190:
         return "High"
-    elif LDL_result >=190:
-       return "Very High"
+    elif LDL_result >= 190:
+        return "Very High"
+
 
 def HDL_analysis(HDL_result):
     if HDL_result >= 60:
         return "Good"
-    elif 40<= HDL_result <60:
+    elif 40 <= HDL_result < 60:
         return "Borderline"
     else:
         return "Bad"
-        
+
+
 def verify_entry(HDL_result):
     if HDL_result[0] != "HDL":
         return False
     if not HDL_result[1].isnumeric():
         return False
     return True
-    
+
+
 def LDL_interface():
     # Input should be LDL=130
     print("LDL Interface")
@@ -35,19 +39,21 @@ def LDL_interface():
     LDL_status = LDL_analysis(int(LDL_result[1]))
     print("LDL status is {}".format(LDL_status))
 
+
 def HDL_interface():
     # Input should be HDL=66
     print("HDL Interface")
     print("Please input the result in the following format: ")
     print("  HDL=## where ## is the numeric result")
     HDL_input = input("Result: ")
-    HDL_result = HDL_input.split("=") 
+    HDL_result = HDL_input.split("=")
     if verify_entry(HDL_result):
         HDL_status = HDL_analysis(int(HDL_result[1]))
         print("HDL status is {}". format(HDL_status))
     else:
         print("Bad entry")
     pass # allows a function to be happy with nothing there- pass is a line of code that doesn't do anything
+
 
 def TC_interface():
     print("Total Cholesterol Interface")
@@ -58,6 +64,7 @@ def TC_interface():
     TC_status = TC_analysis(int(TC_result[1]))
     print("Total Cholesterol status is {}".format(TC_status))
 
+
 def TC_analysis(TC_result):
     if TC_result >= 240:
         return "High"
@@ -66,7 +73,7 @@ def TC_analysis(TC_result):
     else:
         return "Normal"
 
-    
+
 def interface():
     print("My Blood Analysis Calculator")
     keep_running = True # boolean variable- can also use a string
@@ -85,8 +92,32 @@ def interface():
             LDL_interface()
         elif choice == "3":
             TC_interface()
-        
-        
+   
+
+def line_equation(p1,p2):
+    x1,y1 =p1
+    x2,y2 = p2
+    m = (y1-y2)/(x1-x2)
+    b = y1-m*x1
+    return m,b
+    
+def newPoint(m, b, x):
+    y=m*x+b
+    return y
+
+def line_generation(p1,p2,x):
+    return newPoint(line_equation(p1,p2)[0], line_equation(p1,p2)[1], x)
+    
+def line_check(p1,p2,p3):
+    a=line_equation(p1,p2)[0]
+    b=line_equation(p2,p3)[0]
+    if a == b:
+        return True
+    else:
+        return False
+
+    
+
+
 if __name__ == "__main__":
     interface() # calls the function- need to do this in if statement!
-    
